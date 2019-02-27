a2be00f9550e0537b407fa21be5bee5347443ad6
Renan Moura <rvmoura.96@gmail.com>
2019-02-27 14:57:54 -0300

    Implementando método __repr__, o atributo complexo de filhos e um "teste de instanciação de objetos do tipo pessoa".

--- orientacao_objetos/pessoa.py
+++ orientacao_objetos/pessoa.py
@@ -1,9 +1,24 @@
 class Pessoa:
-    def __init__(self, nome=None, idade=None):
+    def __init__(self, *filhos, nome=None, idade=None):
         """Inicializa os atributos da instância."""
         self.nome = nome
         self.idade = idade
+        self.filhos = list(filhos)
+
+    def __repr__(self):
+        """Representa a instancia utilizando seu nome."""
+        return self.nome
 
     def cumprimentar(self):
         """Retorna o cumprimento da instância de pessoa com seu ID."""
         return f'Olá me chamo {self.nome} e tenho {self.idade} anos.'
+
+
+if __name__ == '__main__':
+    filho = Pessoa(nome='Rose')
+    segundo_filho = Pessoa(nome='Shaw')
+    # Os filhos podem ser enviados do jeito abaixo ou então através
+    # de uma tupla.
+    pai = Pessoa(filho, segundo_filho, nome='Evans', idade=50)
+
+    print(pai.filhos)
