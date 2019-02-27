6b9b9482576e366a8175ee7736b1e1d1ea8b79ad
Renan Moura <rvmoura.96@gmail.com>
2019-02-27 14:15:28 -0300

    Adicionando atributo a classe pessoa e melhora no método cumprimentar.

--- orientacao_objetos/pessoa.py
+++ orientacao_objetos/pessoa.py
@@ -1,6 +1,9 @@
 class Pessoa:
+    def __init__(self, nome=None, idade=None):
+        """Inicializa os atributos da instância."""
+        self.nome = nome
+        self.idade = idade
+
     def cumprimentar(self):
-        """
-        Retorna o cumprimento da instância de pessoa com seu ID.
-        """
-        return f'Olá {id(self)}'
+        """Retorna o cumprimento da instância de pessoa com seu ID."""
+        return f'Olá me chamo {self.nome} e tenho {self.idade} anos.'
