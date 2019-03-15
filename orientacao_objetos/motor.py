f819c0690ee735b57b9148d652da53900cb1d27f
Renan Moura <rvmoura.96@gmail.com>
2019-03-15 19:49:29 -0300

    Adicionando enunciado com responsabilidades do motor no módulo motor.

--- orientacao_objetos/motor.py
+++ orientacao_objetos/motor.py
@@ -1,3 +1,12 @@
+"""
+O Motor terá a responsabilidade de controlar a velocidade.
+Ele oferece os seguintes atributos:
+1) Atributo de dado velocidade
+2) Método acelerar, que deverá incrementar a velocidade de uma unidade
+3)_Método frear que deverá decrementar a velocidade em duas unidades.
+"""
+
+
 class Motor:
     def __init__(self):
         """Atributos de dados da classe Motor."""
