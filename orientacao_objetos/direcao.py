b22cb80441e58e4a07a4c54f9c152d367a0f08a7
rvmoura <rvmoura.96@gmail.com>
2019-03-16 11:23:18 -0300

    Implementação da classe direção e seus métodos, iniciada a implementação da classe composta Carro.

--- orientacao_objetos/direcao.py
+++ orientacao_objetos/direcao.py
@@ -1,2 +1,45 @@
+"""
+A direção terá a responsabilidade de controlar a direção. Ela oferece os seguintes atributos:
+1) Valor de direção com valores possíveis: Norte, Sul, Leste, Oeste.
+2) Método girar_a_direita
+3) Método girar_a_esquerda
+"""
+
+
 class Direcao:
-    ...
+    def __init__(self):
+        self.valor = "Norte"
+
+    def girar_a_direita(self):
+        """Deve alterar o valor virando a direita.
+        Deverá alterar o valor da direção para a próxima direção olhando a sua direita,
+        por exemplo:
+        >>> direcao = Direcao()
+        >>> direcao.valor
+        'Norte'
+        >>> direcao.girar_a_direita()
+        >>> direcao.valor
+        'Leste'
+        >>> direcao.girar_a_direita()
+        >>> direcao.valor
+        'Sul'
+        """
+        direcoes = {"Norte": "Leste", "Leste": "Sul", "Sul": "Oeste", "Oeste": "Norte"}
+        self.valor = direcoes.get(self.valor)
+
+    def girar_a_esquerda(self):
+        """Deve alterar o valor virando a esquerda.
+        Deverá alterar o valor da direção para a próxima direção a sua esquerda,
+        por exemplo:
+        >>> direcao = Direcao()
+        >>> direcao.valor
+        'Norte'
+        >>> direcao.girar_a_esquerda()
+        >>> direcao.valor
+        'Oeste'
+        >>> direcao.girar_a_esquerda()
+        >>> direcao.valor
+        'Sul'
+        """
+        direcoes = {"Norte": "Oeste", "Oeste": "Sul", "Sul": "Leste", "Leste": "Norte"}
+        self.valor = direcoes.get(self.valor)
